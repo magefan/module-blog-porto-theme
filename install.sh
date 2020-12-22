8e928906cda0b96df299c44244f2cb7ffbbfb5e8
Magefan <ihoronesuch@gmail.com>
2020-12-22 10:36:26 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-rm magefan-blog-porto-theme.zip
+rm -rf magefan-blog-porto-theme.zip
+rm -rf magefan-blog-porto-theme
+wget -O magefan-blog-porto-theme.zip https://github.com/magefan/module-blog-porto-theme/archive/main.zip
+unzip magefan-blog-porto-theme.zip -d magefan-blog-porto-theme
+rm magefan-blog-porto-theme/install.sh
 cd var
 mkdir backup
 cd ..
 mv app/design/frontend/Smartwave/porto/Magefan_Blog "var/backup/Magefan_Blog_old_`date`"
 mv magefan-blog-porto-theme app/design/frontend/Smartwave/porto/Magefan_Blog
-echo "Magefan Blog extension templates in Porto Theme where updated. Old files were backuped and moved to var/backup/Magefan_Blog_old"
-echo "Now please redeploy static content using the command bin/magento setup:static-content:deploy -f"
 rm -rf magefan-blog-porto-theme.zip
 rm -rf magefan-blog-porto-theme
+echo "Magefan Blog extension templates in Porto Theme where updated. Old files were backuped and moved to var/backup/Magefan_Blog_old"
+echo "Now please redeploy static content using the command bin/magento setup:static-content:deploy -f"
